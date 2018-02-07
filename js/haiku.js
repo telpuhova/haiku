a6738d922542f50ab49f4e608a21685fb1f6e3ad
Guest User <Guest@Epicodus-4E.local>
2018-02-07 10:22:19 -0800

    add checking line count to backend

---
+++ js/haiku.js
@@ -0,0 +1,21 @@
+class Haiku {
+  constructor(poem) {
+    this.poem = poem;
+  }
+
+  check() {
+    let lines = this.poem.split("");
+    let count = 0;
+    for (let i = 0; i < lines.length; i++){
+      if (lines[i] === '\n') {
+        count = count + 1;
+      }
+    }
+    if (count === 2) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+ exports.haikuModule=Haiku;
---
+++ spec/haiku-spec.js
@@ -0,0 +1,20 @@
+var Haiku = require('./../js/haiku.js').haikuModule;
+
+describe('Haiku', function() {
+  it('should return true or false', function(){
+    var poem = `hello
+    how are you
+    doing well`;
+    var notPoem = `this is
+    not a haiku
+    because
+    it has
+    more lines`;
+    var sample1 = new Haiku(poem);
+    var sample2 = new Haiku(notPoem);
+    expect(sample1.check()).toEqual(true);
+    expect(sample2.check()).toEqual(false);
+  });
+
+
+});
