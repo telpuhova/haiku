bbcc94946b97516e875cfe5b24202be4f22fc2ff
Guest User <Guest@Epicodus-4E.local>
2018-02-07 15:16:57 -0800

    add test

--- js/haiku.js
+++ js/haiku.js
@@ -3,7 +3,7 @@ export class Haiku {
     this.poem = poem;
   }
 
-  check() {
+  checkLines() {
     let lines = this.poem.split("");
     let count = 0;
     for (let i = 0; i < lines.length; i++){
@@ -16,6 +16,68 @@ export class Haiku {
     } else {
       return false;
     }
+  }
+
+  checkSyllables() {
+    // debugger;
+
+    this.poem = this.poem.toLowerCase();
+    const vowels = ['a', 'e', 'y', 'u', 'i', 'o'];
+    const exceptions = ['s', 'c', 'x', 'g', 'z'];
+    let lines = this.poem.split("\n");
+
+    let wordSliced = "";
+    const requiredAmountOfSyllables = [5, 7, 5];
+    for (let i=0; i<lines.length; i++) {
+      let syllables = 0;
+      console.log(lines[i]);
+      let words = lines[i].split(" ");
+      for (let j=0; j<words.length; j++) {
+        console.log(words[j]);
+        wordSliced = words[j];
+        if (words[j].endsWith("less")) {
+          syllables = syllables + 1;
+          wordSliced = words[j].slice(0, (words[j].length - 4));
+        }
+        if (words[j].endsWith("ful")) {
+          syllables = syllables + 1;
+          wordSliced = words[j].slice(0, (words[j].length - 3));
+        }
+        // old words, where le sounds like an 'el'
+        if (words[j].endsWith("ble")) {
+          syllables =syllables + 1;
+        } else if (words[j].endsWith("e")) {// silent e at the end of a word
+          wordSliced = words[j].slice(0, (words[j].length - 1));
+        }
+        // plural form with a silent e
+        if (words[j].endsWith("es")) {
+          if (!exceptions.includes(words[j].charAt(words[j].length - 3))) {
+            wordSliced = words[j].slice(0, (words[j].length - 2));
+          }
+        }
+        let letters = wordSliced.split("");
+        console.log(letters);
+        let isPreviousLetterAVowel = false;
+        for (let k=0; k<letters.length; k++) {
+
+          if (vowels.includes(letters[k]) && (isPreviousLetterAVowel === false)) {
+            console.log("if: vowel; f=false");
+            isPreviousLetterAVowel = true;
+            syllables = syllables + 1;
+          } else if (!vowels.includes(letters[k])) {
+            console.log("if: consonant;");
+            isPreviousLetterAVowel = false;
+          } else {
+
+          }
+        }
+        console.log(syllables);
+      }
+      if (syllables != requiredAmountOfSyllables[i]) {
+        return false;
+      }
+    }
+    return true;
   }
 }
  // exports.haikuModule=Haiku;
--- spec/haiku-spec.js
+++ spec/haiku-spec.js
@@ -3,19 +3,47 @@ import { Haiku } from './../js/haiku.js';
 
 describe('Haiku', function() {
   it('should return true or false', function(){
-    var poem = `hello
+    let poem = `hello
     how are you
     doing well`;
-    var notPoem = `this is
+    let notPoem = `this is
     not a haiku
     because
     it has
     more lines`;
-    var sample1 = new Haiku(poem);
-    var sample2 = new Haiku(notPoem);
-    expect(sample1.check()).toEqual(true);
-    expect(sample2.check()).toEqual(false);
+    let sample1 = new Haiku(poem);
+    let sample2 = new Haiku(notPoem);
+    expect(sample1.checkLines()).toEqual(true);
+    expect(sample2.checkLines()).toEqual(false);
   });
 
+  it('should check number of syllables correctly', function(){
+    let poem = `In a pouch I grow,
+    On a southern continent
+    Strange creatures I know.`
+    let lineOneFail = `In a pouch I grow alot,
+    On a southern continent
+    Strange creatures I know.`
+    let lineTwoFail =`In a pouch I grow,
+    On a big southern continent
+    Strange creatures I know.`
+    let lineThreeFail = `In a pouch I grow,
+    On a southern continent
+    Strange creatures I know hi.`
+    let notPoem = `I am first with five
+    Then seven in the middle
+    Five again to end.`
+    let sample1 = new Haiku(poem);
+    let sample2 = new Haiku(lineOneFail);
+    let sample3 = new Haiku(lineTwoFail);
+    let sample4 = new Haiku(lineThreeFail);
+    let sample5 = new Haiku(notPoem);
+    expect(sample1.checkSyllables()).toEqual(true);
+    // expect(sample2.checkSyllables()).toEqual(false);
+    // expect(sample3.checkSyllables()).toEqual(false);
+    // expect(sample4.checkSyllables()).toEqual(false);
+    // expect(sample5.checkSyllables()).toEqual(false);
+
+  })
 
 });
