643a161b67def8096b48a07b45accf8b8ec9f563
Guest User <Guest@Epicodus-4E.local>
2018-02-07 16:29:15 -0800

    add installations(gulp)

--- build/js/app.js
+++ build/js/app.js
@@ -1,23 +1,4 @@
 (function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
-"use strict";
-
-var _haiku = require("./../js/haiku.js");
-
-$(document).ready(function () {
-  $("#userInput").submit(function (event) {
-    event.preventDefault();
-    var poem = $("#poem").val();
-
-    var haiku = new _haiku.Haiku(poem);
-    if (haiku.check()) {
-      $(".output").text("this is a haiku");
-    } else {
-      $(".output").text("this is NOT a haiku");
-    }
-  });
-});
-
-},{"./../js/haiku.js":2}],2:[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -129,4 +110,23 @@ var Haiku = exports.Haiku = function () {
 }();
 // exports.haikuModule=Haiku;
 
-},{}]},{},[1]);
+},{}],2:[function(require,module,exports){
+"use strict";
+
+var _haiku = require("./../js/haiku.js");
+
+$(document).ready(function () {
+  $("#userInput").submit(function (event) {
+    event.preventDefault();
+    var poem = $("#poem").val();
+
+    var haiku = new _haiku.Haiku(poem);
+    if (haiku.check()) {
+      $(".output").text("this is a haiku");
+    } else {
+      $(".output").text("this is NOT a haiku");
+    }
+  });
+});
+
+},{"./../js/haiku.js":1}]},{},[2]);
--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,55 @@
 var gulp =require('gulp');
 var browserify =require('browserify');
 var source = require('vinyl-source-stream');
-// var concat =require('gulp-concat');
-// var utilities =require('gulp-util');
-// var del =require('del');
-// var jshint =require('gulp-jshint');
-// var buildProduction =utilites.env.production;
+var concat =require('gulp-concat');
+var uglify = require('gulp-uglify');
+var utilities = require('gulp-util');
+var del =require('del');
+var jshint =require('gulp-jshint');
+var buildProduction = utilities.env.production;
 var babelify = require("babelify");
 
+gulp.task('build',function(){
+  if (buildProduction) {
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start ('jsBrowserify');
+  }
+})
 
-
-gulp.task('jsBrowserify', function() {
-  return browserify({ entries: ['./js/haiku-interface.js']})
+gulp.task('jsBrowserify',['concat'], function() {
+  return browserify({ entries: ['./tmp/allConcat.js']})
     .transform(babelify.configure({
       presets: ["es2015"]
     }))
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'))
+});
+gulp.task('concat', function() {
+  return gulp.src(['./js/haiku-interface.js'])
+  .pipe(concat('allConcat.js'))
+  .pipe(gulp.dest("./tmp"));
+});
+gulp.task("minifyScripts",["jsBrowserify"],function(){
+  return gulp.src(".build/js/app.js")
+  .pipe(uglify())
+  .pipe(gulp.dest("./build/js"));
+});
+
+gulp.task("clean", function() {
+  return del(['build','tmp']);
+});
+
+gulp.task('build', ['clean'], function() {
+  if(buildProduction){
+    gulp.start('minifyScripts');
+  } else {
+    gulp.start('jsBrowserify');
+  }
+});
+gulp.task('jshint',function(){
+  return gulp.src(['js/*.js'])
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'));
 });
